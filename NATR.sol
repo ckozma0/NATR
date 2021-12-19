ff77c59fcce99539fa67a7551a16cf0540999c13
Chris Kozma <54963404+ckozma0@users.noreply.github.com>
2021-12-18 22:44:22 -0500

    Update NATR.sol

--- NATR.sol
+++ NATR.sol
@@ -4,12 +4,12 @@ pragma solidity ^0.8.2;
 import "@openzeppelin/contracts-upgradeable@4.4.0/token/ERC20/ERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable@4.4.0/proxy/utils/Initializable.sol";
 
-contract KozCoin is Initializable, ERC20Upgradeable {
+contract NATR is Initializable, ERC20Upgradeable {
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() initializer {}
 
     function initialize() initializer public {
-        __ERC20_init("KozCoin", "KOZ");
+        __ERC20_init("NatureToken", "NATR");
 
         _mint(msg.sender, 10000000 * 10 ** decimals());
     }
